619bf74ce0b9e701246164c39cd98a47eb184722
DESKTOP-15FTLV4\marti <tongdaitructuyen@gmail.com>
2018-08-11 16:53:30 +0700

    new_update

---
+++ tidioh39.js
@@ -0,0 +1,25 @@
+function hideTdo() {
+  var timer = null;
+  var target = document.querySelectorAll('#tidio-chat-code')[0];
+  if(!target || typeof target === 'undefined') {
+    if(timer !== null) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(hideTdo, 500);
+    return;
+  } else {
+    var timer2 = null;
+    var tdo = document.querySelectorAll('#tidio-chat-code')[0].contentWindow.document.querySelectorAll('#root > div > div.online > div.input-wrapper > a.powered')[0];
+    if(!tdo || typeof tdo === 'undefined') {
+      if(timer2 !== null) {
+        clearTimeout(timer2);
+      }
+      timer2 = setTimeout(hideTdo, 1);
+      return;
+    }
+    document.querySelectorAll('#tidio-chat-code')[0].contentWindow.document.querySelectorAll('#root > div > div.online > div.input-wrapper > a.powered')[0].style.display = 'none';
+    return true;
+  }
+}
+
+hideTdo();
