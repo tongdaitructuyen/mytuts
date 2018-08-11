d0abca3896e8bd6b2032a4c135e4b2bee56a61f4
Martino Hugo <tongdaitructuyen@gmail.com>
2018-08-11 17:01:06 +0700

    Update Online
    Yes

--- tidioh.js
+++ tidioh.js
@@ -1,6 +1,6 @@
 function hideTdo() {
   var timer = null;
-  var target = document.querySelectorAll('#tidio-chat-code')[0];
+  var target = document.querySelectorAll('#frame-content')[0];
   if(!target || typeof target === 'undefined') {
     if(timer !== null) {
       clearTimeout(timer);
@@ -9,7 +9,7 @@ function hideTdo() {
     return;
   } else {
     var timer2 = null;
-    var tdo = document.querySelectorAll('#tidio-chat-code')[0].contentWindow.document.querySelectorAll('#root > div > div.online > div.input-wrapper > a.powered')[0];
+    var tdo = document.querySelectorAll('#frame-content')[0].contentWindow.document.querySelectorAll('#root > div > div.online > div.input-wrapper > a.powered')[0];
     if(!tdo || typeof tdo === 'undefined') {
       if(timer2 !== null) {
         clearTimeout(timer2);
@@ -17,7 +17,7 @@ function hideTdo() {
       timer2 = setTimeout(hideTdo, 1);
       return;
     }
-    document.querySelectorAll('#tidio-chat-code')[0].contentWindow.document.querySelectorAll('#root > div > div.online > div.input-wrapper > a.powered')[0].style.display = 'none';
+    document.querySelectorAll('#frame-content')[0].contentWindow.document.querySelectorAll('#root > div > div.online > div.input-wrapper > a.powered')[0].style.display = 'none';
     return true;
   }
 }
